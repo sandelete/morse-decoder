06c5f01a7a003323e8462f614fa545d7430aeedb
Саша Ряхов <rkhv@MacBook-Pro-Sasa.local>
2020-10-19 19:07:28 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,42 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ':  ' ',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(str) {
+    let arr = [];
+    let result = '';
+
+    for (let i = 0; i < str.length ; i += 10) {
+        arr.push(str.slice(i, i + 10));
+    }
+
+    let newarr = [];
+    
+    for (item of arr) {
+        if (item === '**********') {
+            newarr.push(' ');
+        } else {
+            let rem = '';
+            for (let j = 0; j < item.length; j += 2) {
+                if (item[j] === '1' && item[j + 1] === '1') {
+                    rem += '-';
+                }
+                if (item[j] === '1' && item[j + 1] === '0') {
+                    rem += '.';
+                } else {
+                    item.replace('00', '');
+                }
+            }
+            newarr.push(rem);
+        }
+    }
+
+    for (item of newarr) {
+        result += MORSE_TABLE[item];
+    }
+    return result;
 }
 
 module.exports = {
